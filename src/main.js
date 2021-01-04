9e18090d8a25fba281c049fac499c5a48fcd7f92
Erin Ringland <erinringland@gmail.com>
2021-01-04 12:31:01 -0600

    Added Pagination

--- src/main.js
+++ src/main.js
@@ -1,7 +1,15 @@
 const TESTING = false;
 
+let currentPage = 1;
+
+let currentSearch = 'Star Wars';
+
+let currentResults = 10;
+
 const moviesList = document.getElementById('movies');
 const movieInput = document.getElementById('search');
+
+const pagination = document.getElementById('pagination');
 
 const omdbURL = new URL('https://www.omdbapi.com');
 const params = new URLSearchParams();
@@ -10,47 +18,105 @@ params.append('apikey', 'ae7826ed');
 movieInput.addEventListener('keyup', search);
 getMovies();
 
+pagination.addEventListener('click', function (e) {
+  console.log(currentPage);
+  console.log(currentResults);
+  let target = e.target.id;
+  if (target === 'prev') {
+    if (currentPage > 1) {
+      currentPage--;
+      getMovies();
+    }
+  } else if (target === 'next') {
+    if (currentPage < getLastPage()) {
+      currentPage++;
+      getMovies();
+    }
+  }
+});
+
+function getLastPage(){
+  return Math.ceil(currentResults / 10);
+}
+
 function showMovies(movies) {
+  console.log(movies);
   moviesList.innerHTML = movies
-    .map(
-      movie => `<li>
+    .map(movie => {
+      let ratings = movie.Ratings;
+      if (ratings.length > 0) {
+        rating = `<div class="rating">${ratings[0].Value}</div>`;
+      } else {
+        rating = '';
+      }
+      return `<li>
       <img src="${movie.Poster}">
       <div class="info">
         <div class="title">${movie.Title}</div>
-        <div class="rating">${movie.Ratings[0].Value}</div>
+        ${rating}
         <div class="plot">${movie.Plot}</div>
       </div>
-      </li>`
-    )
+      </li>`;
+    })
     .join('');
 }
 
-function getUrlWithParam(name, value) {
-  params.append(name, value);
+function getUrlWithParam(options) {
+  for (let [name, value] of Object.entries(options)) {
+    params.append(name, value);
+  }
   omdbURL.search = params;
   let url = omdbURL.href;
-  params.delete(name);
+  for (let [name, value] of Object.entries(options)) {
+    params.delete(name);
+  }
   return url;
 }
 
-function getMovies(search = 'star wars') {
-  fetch(TESTING ? '/star_wars.json' : getUrlWithParam('s', search))
+function showPagination() {
+  pagination.innerHTML = '';
+  let prev = `<button id="prev">Previous</button>`;
+  let next = `<button id="next">Next</button>`;
+  if (currentResults > 10 && currentPage < getLastPage()) {
+    pagination.insertAdjacentHTML('beforeend', next);
+  } 
+  if (currentPage > 1) {
+    pagination.insertAdjacentHTML('afterbegin', prev);
+  }
+  pagination.insertAdjacentHTML(
+    'beforeend',
+    `<span>${currentPage} of ${getLastPage()}</span>`
+  );
+}
+
+function getMovies() {
+  fetch(
+    TESTING
+      ? '/star_wars.json'
+      : getUrlWithParam({s: currentSearch, page: currentPage})
+  )
     .then(response => response.json())
-    .then(result =>
+    .then(result => {
+      currentResults = result.totalResults;
+      showPagination();
+
       Promise.all(
         result.Search.map(movie => movie.imdbID).map(id =>
           fetch(
-            TESTING ? '/new_hope.json' : getUrlWithParam('i', id)
+            TESTING ? '/new_hope.json' : getUrlWithParam({i: id})
           ).then(response => response.json())
         )
-      ).then(showMovies)
-    );
+      ).then(showMovies);
+    });
 }
 
 function search(event) {
   let movie = movieInput.value;
   if (event.key === 'Enter' && movie !== '') {
-    getMovies(movie);
+    currentSearch = movie;
+    currentPage = 1;
+    getMovies();
+    showPagination();
     movieInput.value = '';
   }
 }
